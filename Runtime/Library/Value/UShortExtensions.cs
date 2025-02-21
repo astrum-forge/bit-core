a0ba8cd9939af39e803599edc13a377a0ece90ab
David Arayan <david.arayan12@gmail.com>
2025-02-21 14:11:25 +0400

    - Improved Unsigned Short Extensions for consistency and performance

--- Runtime/Library/Value/UShortExtensions.cs
+++ Runtime/Library/Value/UShortExtensions.cs
@@ -6,304 +6,269 @@
 #define BITCORE_METHOD_INLINE
 #endif
 
-using System;
 #if BITCORE_METHOD_INLINE
 using System.Runtime.CompilerServices;
 #endif
-using System.Text;
+
+using System;
 
 namespace BitCore
 {
 	/// <summary>
-	/// Provides extension methods for the <see cref="ushort"/> type.
-	/// A <see cref="ushort"/> is an unsigned 16‑bit integer.
-	/// <para>
-	/// In debug or editor builds, error checks are enabled; these are removed in production.
-	/// </para>
-	/// <para>
-	/// Critical Change (20/12/2018): For .NET 4.6 targets, functions are hinted for aggressive inlining.
-	/// </para>
+	/// Provides high-performance extension methods for the <see cref="ushort"/> type, optimized for bit manipulation and conversions.
+	/// <para>A <see cref="ushort"/> is an unsigned 16-bit integer (0 to 65535).</para>
+	/// <para>See also: <see href="https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/ushort">ushort keyword</see>.</para>
+	/// <para><b>Performance Note:</b> Methods are aggressively inlined in .NET 4.6+ builds. Debug checks are included in development builds and stripped in release for maximum speed.</para>
+	/// <para><b>Change History:</b>
+	/// <list type="bullet">
+	///   <item>20/12/2018: Added AggressiveInlining for .NET 4.6 targets.</item>
+	/// </list></para>
 	/// </summary>
 	public static class UShortExtensions
 	{
 		/// <summary>
-		/// Returns <c>true</c> if the <see cref="ushort"/> value is greater than zero.
+		/// Converts the <see cref="ushort"/> value to a boolean.
 		/// </summary>
 		/// <param name="data">The ushort value.</param>
-		/// <returns><c>true</c> if the value is greater than zero; otherwise, <c>false</c>.</returns>
+		/// <returns>True if the value is greater than zero; otherwise, false.</returns>
 #if BITCORE_METHOD_INLINE
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
 		public static bool Bool(this ushort data) => data > 0;
 
 		/// <summary>
-		/// Retrieves the bit (0 or 1) at the specified position in the ushort value.
-		/// The position must be between 0 (least significant) and 15 (most significant).
+		/// Gets the bit value (0 or 1) at the specified position.
 		/// </summary>
 		/// <param name="data">The ushort value.</param>
-		/// <param name="pos">The bit position (0–15).</param>
-		/// <returns>The bit (0 or 1) at the specified position.</returns>
+		/// <param name="pos">The bit position (0 to 15).</param>
+		/// <returns>1 if the bit is set; 0 if cleared.</returns>
+		/// <exception cref="ArgumentOutOfRangeException">In debug mode, thrown if pos is not 0-15.</exception>
 #if BITCORE_METHOD_INLINE
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-		public static int BitAt(this ushort data, int pos)
+		public static ushort BitAt(this ushort data, int pos)
 		{
 #if BITCORE_DEBUG
-            if (pos < 0 || pos > 15)
-            {
-                BitDebug.Throw($"ushort.BitAt(int) - position must be between 0 and 15 but was {pos}");
-            }
+			if (pos < 0 || pos > 15) throw new ArgumentOutOfRangeException(nameof(pos), $"Position must be 0-15, was {pos}.");
 #endif
-			return (data >> pos) & 1;
+			return (ushort)((data >> pos) & 1);
 		}
 
 		/// <summary>
-		/// Returns the inverted bit at the specified position.
-		/// For a given position, if the bit is 1, it returns 0; if 0, it returns 1.
-		/// The position must be between 0 and 15.
+		/// Gets the inverted bit value (0 or 1) at the specified position.
 		/// </summary>
 		/// <param name="data">The ushort value.</param>
-		/// <param name="pos">The bit position (0–15).</param>
-		/// <returns>The inverted bit (0 or 1) at the specified position.</returns>
+		/// <param name="pos">The bit position (0 to 15).</param>
+		/// <returns>0 if the bit is set; 1 if cleared.</returns>
+		/// <exception cref="ArgumentOutOfRangeException">In debug mode, thrown if pos is not 0-15.</exception>
 #if BITCORE_METHOD_INLINE
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-		public static int BitInvAt(this ushort data, int pos)
+		public static ushort BitInvAt(this ushort data, int pos)
 		{
 #if BITCORE_DEBUG
-            if (pos < 0 || pos > 15)
-            {
-                BitDebug.Throw($"ushort.BitInvAt(int) - position must be between 0 and 15 but was {pos}");
-            }
+			if (pos < 0 || pos > 15) throw new ArgumentOutOfRangeException(nameof(pos), $"Position must be 0-15, was {pos}.");
 #endif
-			return 1 - ((data >> pos) & 1);
+			return (ushort)((~data >> pos) & 1);
 		}
 
 		/// <summary>
 		/// Sets the bit at the specified position to 1.
-		/// The position must be between 0 and 15.
 		/// </summary>
 		/// <param name="data">The ushort value.</param>
-		/// <param name="pos">The bit position (0–15).</param>
-		/// <returns>A new ushort with the bit at the specified position set to 1.</returns>
+		/// <param name="pos">The bit position (0 to 15).</param>
+		/// <returns>A new ushort with the bit set.</returns>
+		/// <exception cref="ArgumentOutOfRangeException">In debug mode, thrown if pos is not 0-15.</exception>
 #if BITCORE_METHOD_INLINE
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
 		public static ushort SetBitAt(this ushort data, int pos)
 		{
 #if BITCORE_DEBUG
-            if (pos < 0 || pos > 15)
-            {
-                BitDebug.Throw($"ushort.SetBitAt(int) - position must be between 0 and 15 but was {pos}");
-            }
+			if (pos < 0 || pos > 15) throw new ArgumentOutOfRangeException(nameof(pos), $"Position must be 0-15, was {pos}.");
 #endif
-			return (ushort)(data | (1u << pos));
+			return (ushort)(data | (1 << pos));
 		}
 
 		/// <summary>
-		/// Clears (sets to 0) the bit at the specified position.
-		/// The position must be between 0 and 15.
+		/// Clears the bit at the specified position to 0.
 		/// </summary>
 		/// <param name="data">The ushort value.</param>
-		/// <param name="pos">The bit position (0–15).</param>
-		/// <returns>A new ushort with the specified bit cleared.</returns>
+		/// <param name="pos">The bit position (0 to 15).</param>
+		/// <returns>A new ushort with the bit cleared.</returns>
+		/// <exception cref="ArgumentOutOfRangeException">In debug mode, thrown if pos is not 0-15.</exception>
 #if BITCORE_METHOD_INLINE
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-		public static ushort UnsetBitAt(this ushort data, int pos)
+		public static ushort ClearBitAt(this ushort data, int pos)
 		{
 #if BITCORE_DEBUG
-            if (pos < 0 || pos > 15)
-            {
-                BitDebug.Throw($"ushort.UnsetBitAt(int) - position must be between 0 and 15 but was {pos}");
-            }
+			if (pos < 0 || pos > 15) throw new ArgumentOutOfRangeException(nameof(pos), $"Position must be 0-15, was {pos}.");
 #endif
-			return (ushort)(data & ~(1u << pos));
+			return (ushort)(data & ~(1 << pos));
 		}
 
 		/// <summary>
-		/// Toggles the bit at the specified position.
-		/// The position must be between 0 and 15.
+		/// Toggles the bit at the specified position (1 to 0, or 0 to 1).
 		/// </summary>
 		/// <param name="data">The ushort value.</param>
-		/// <param name="pos">The bit position (0–15).</param>
-		/// <returns>A new ushort with the specified bit toggled.</returns>
+		/// <param name="pos">The bit position (0 to 15).</param>
+		/// <returns>A new ushort with the bit toggled.</returns>
+		/// <exception cref="ArgumentOutOfRangeException">In debug mode, thrown if pos is not 0-15.</exception>
 #if BITCORE_METHOD_INLINE
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
 		public static ushort ToggleBitAt(this ushort data, int pos)
 		{
 #if BITCORE_DEBUG
-            if (pos < 0 || pos > 15)
-            {
-                BitDebug.Throw($"ushort.ToggleBitAt(int) - position must be between 0 and 15 but was {pos}");
-            }
+			if (pos < 0 || pos > 15) throw new ArgumentOutOfRangeException(nameof(pos), $"Position must be 0-15, was {pos}.");
 #endif
-			return (ushort)(data ^ (1u << pos));
+			return (ushort)(data ^ (1 << pos));
 		}
 
 		/// <summary>
-		/// Sets the bit at the specified position to the provided value (0 or 1).
-		/// The position must be between 0 and 15.
+		/// Sets the bit at the specified position to the given value (0 or 1).
 		/// </summary>
 		/// <param name="data">The ushort value.</param>
-		/// <param name="pos">The bit position (0–15).</param>
-		/// <param name="bit">The bit value to set (0 or 1).</param>
-		/// <returns>A new ushort with the specified bit updated.</returns>
+		/// <param name="pos">The bit position (0 to 15).</param>
+		/// <param name="bit">The bit value (0 or 1).</param>
+		/// <returns>A new ushort with the bit set to the specified value.</returns>
+		/// <exception cref="ArgumentOutOfRangeException">In debug mode, thrown if pos is not 0-15 or bit is not 0/1.</exception>
 #if BITCORE_METHOD_INLINE
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-		public static ushort SetBit(this ushort data, int pos, int bit)
+		public static ushort SetBitValueAt(this ushort data, int pos, int bit)
 		{
 #if BITCORE_DEBUG
-            if (pos < 0 || pos > 15)
-            {
-                BitDebug.Throw($"ushort.SetBit(int, int) - position must be between 0 and 15 but was {pos}");
-            }
-            if (bit != 0 && bit != 1)
-            {
-                BitDebug.Throw($"ushort.SetBit(int, int) - bit value must be either 0 or 1 but was {bit}");
-            }
+			if (pos < 0 || pos > 15) throw new ArgumentOutOfRangeException(nameof(pos), $"Position must be 0-15, was {pos}.");
+			if (bit != 0 && bit != 1) throw new ArgumentOutOfRangeException(nameof(bit), $"Bit value must be 0 or 1, was {bit}.");
 #endif
-			int mask = 1 << pos;
-			int m1 = (bit << pos) & mask;
-			int m2 = data & ~mask;
-			return (ushort)(m2 | m1);
+			return (ushort)(data & ~(1 << pos) | (bit << pos));
 		}
 
 		/// <summary>
-		/// Returns the number of bits set to 1 in the ushort value.
-		/// Uses a general-purpose Hamming Weight (popcount) algorithm.
+		/// Counts the number of bits set to 1 in the ushort (population count).
 		/// </summary>
 		/// <param name="data">The ushort value.</param>
-		/// <returns>The population count (number of set bits).</returns>
+		/// <returns>The number of 1 bits (0 to 16).</returns>
+		/// <remarks>Uses a parallel bit summation algorithm (Hamming Weight) for efficiency.</remarks>
 #if BITCORE_METHOD_INLINE
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-		public static int PopCount(this ushort data) =>
-			((uint)data).PopCount();
+		public static int PopCount(this ushort data)
+		{
+			return ((uint)data).PopCount();
+		}
 
 		/// <summary>
-		/// Determines whether the ushort value is a power of two.
+		/// Determines if the ushort is a power of two.
 		/// </summary>
 		/// <param name="value">The ushort value.</param>
-		/// <returns><c>true</c> if the value is a power of two; otherwise, <c>false</c>.</returns>
+		/// <returns>True if the value is a power of 2 (1, 2, 4, 8, etc.); otherwise, false.</returns>
 #if BITCORE_METHOD_INLINE
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-		public static bool IsPowerOfTwo(this ushort value) =>
-			value != 0 && (value & (value - 1)) == 0;
+		public static bool IsPowerOfTwo(this ushort value) => value != 0 && (value & (value - 1)) == 0;
 
 		/// <summary>
-		/// Retrieves the byte at the specified position from the ushort value.
-		/// Position 0 returns the high byte, and 1 returns the low byte.
+		/// Retrieves the byte at the specified position within the ushort (big-endian order).
 		/// </summary>
 		/// <param name="data">The ushort value.</param>
-		/// <param name="pos">The byte position (0–1).</param>
+		/// <param name="pos">The byte position (0 or 1, where 0 is the most significant byte).</param>
 		/// <returns>The byte at the specified position.</returns>
+		/// <exception cref="ArgumentOutOfRangeException">In debug mode, thrown if pos is not 0-1.</exception>
 #if BITCORE_METHOD_INLINE
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
 		public static byte ByteAt(this ushort data, int pos)
 		{
 #if BITCORE_DEBUG
-            if (pos < 0 || pos > 1)
-            {
-                BitDebug.Throw($"ushort.ByteAt(int) - position must be between 0 and 1 but was {pos}");
-            }
+			if (pos < 0 || pos > 1) throw new ArgumentOutOfRangeException(nameof(pos), $"Position must be 0-1, was {pos}.");
 #endif
 			return (byte)(data >> (8 - (pos * 8)));
 		}
 
 		/// <summary>
-		/// Replaces the byte at the specified position in the ushort value with a new byte.
-		/// Position 0 replaces the high byte, and 1 replaces the low byte.
+		/// Sets the byte at the specified position within the ushort (big-endian order).
 		/// </summary>
 		/// <param name="data">The ushort value.</param>
-		/// <param name="newData">The new byte to set.</param>
-		/// <param name="pos">The byte position (0–1).</param>
-		/// <returns>A new ushort with the specified byte replaced.</returns>
+		/// <param name="newData">The new byte value.</param>
+		/// <param name="pos">The byte position (0 or 1, where 0 is the most significant byte).</param>
+		/// <returns>A new ushort with the byte replaced.</returns>
+		/// <exception cref="ArgumentOutOfRangeException">In debug mode, thrown if pos is not 0-1.</exception>
 #if BITCORE_METHOD_INLINE
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
 		public static ushort SetByteAt(this ushort data, byte newData, int pos)
 		{
-#if BITCORE_METHOD_INLINE
-            // (Using BITCORE_DEBUG check)
-#endif
 #if BITCORE_DEBUG
-            if (pos < 0 || pos > 1)
-            {
-                BitDebug.Throw($"ushort.SetByteAt(int) - position must be between 0 and 1 but was {pos}");
-            }
+			if (pos < 0 || pos > 1) throw new ArgumentOutOfRangeException(nameof(pos), $"Position must be 0-1, was {pos}.");
 #endif
 			int shift = 8 - (pos * 8);
-			int mask = 0xFF << shift;
-			int m1 = (newData << shift) & mask;
-			int m2 = data & ~mask;
-			return (ushort)(m2 | m1);
+			ushort mask = (ushort)(0xFF << shift);
+			return (ushort)((data & ~mask) | (newData << shift));
 		}
 
 		/// <summary>
-		/// Returns a 16-character string representing the binary form of the ushort value.
-		/// The string is composed of '0' and '1' characters for bits 15 (MSB) to 0 (LSB).
+		/// Returns the binary string representation of the ushort (16 characters of '0' or '1').
 		/// </summary>
 		/// <param name="value">The ushort value.</param>
-		/// <returns>A 16-character binary string.</returns>
+		/// <returns>A 16-character string of bits, e.g., "0000000000001111" for 15.</returns>
+		/// <remarks>For performance-critical code, consider avoiding string allocation.</remarks>
 #if BITCORE_METHOD_INLINE
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
 		public static string BitString(this ushort value)
 		{
-			var sb = new StringBuilder(16);
+			char[] bits = new char[16];
 			for (int i = 15; i >= 0; i--)
 			{
-				sb.Append(value.BitAt(i));
+				bits[15 - i] = (char)('0' + ((value >> i) & 1));
 			}
-			return sb.ToString();
+			return new string(bits);
 		}
 
 		/// <summary>
-		/// Converts 16 characters from a binary string, starting at the specified index, into a ushort value.
+		/// Converts a 16-character substring of binary digits starting at <paramref name="readIndex"/> into a ushort.
 		/// </summary>
-		/// <param name="data">A binary string representing bits.</param>
-		/// <param name="readIndex">The starting index from which to read 16 characters.</param>
-		/// <returns>A ushort corresponding to the binary string.</returns>
+		/// <param name="data">The string of '0' and '1' characters.</param>
+		/// <param name="readIndex">The starting index (must allow 16 characters).</param>
+		/// <returns>The ushort value represented by the substring.</returns>
+		/// <exception cref="ArgumentOutOfRangeException">In debug mode, thrown if readIndex is invalid.</exception>
 #if BITCORE_METHOD_INLINE
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
 		public static ushort UShortFromBitString(this string data, int readIndex)
 		{
 #if BITCORE_DEBUG
-            if ((readIndex + 16) > data.Length)
-            {
-                BitDebug.Throw("string.UShortFromBitString(int) - read index and ushort length exceed string length");
-            }
+			if (readIndex < 0 || readIndex + 16 > data.Length)
+				throw new ArgumentOutOfRangeException(nameof(readIndex), $"readIndex + 16 ({readIndex + 16}) exceeds string length ({data.Length}).");
 #endif
 			ushort value = 0;
-			for (int i = readIndex, j = 15; i < readIndex + 16; i++, j--)
+			for (int i = 0; i < 16; i++)
 			{
-				value = data[i] == '1' ? value.SetBitAt(j) : value.UnsetBitAt(j);
+				value = (ushort)((value << 1) | (data[readIndex + i] - '0'));
 			}
 			return value;
 		}
 
 		/// <summary>
-		/// Converts the first 16 characters of a binary string into a ushort value.
+		/// Converts the first 16 characters of a binary string into a ushort.
 		/// </summary>
-		/// <param name="data">A binary string representing 16 bits.</param>
-		/// <returns>A ushort corresponding to the binary string.</returns>
+		/// <param name="data">The string of '0' and '1' characters.</param>
+		/// <returns>The ushort value from the first 16 characters.</returns>
+		/// <exception cref="ArgumentOutOfRangeException">In debug mode, thrown if string is too short.</exception>
 #if BITCORE_METHOD_INLINE
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-		public static ushort UShortFromBitString(this string data) =>
-			data.UShortFromBitString(0);
+		public static ushort UShortFromBitString(this string data) => data.UShortFromBitString(0);
 
 		/// <summary>
-		/// Returns the hexadecimal string representation of the ushort value.
+		/// Returns the hexadecimal string representation of the ushort.
 		/// </summary>
 		/// <param name="value">The ushort value.</param>
-		/// <returns>A hexadecimal string representation.</returns>
+		/// <returns>A string like "FFFF" for 65535.</returns>
+		/// <remarks>For performance-critical code, consider avoiding string allocation.</remarks>
 #if BITCORE_METHOD_INLINE
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
